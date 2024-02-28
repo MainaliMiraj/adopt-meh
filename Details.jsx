b034ee0a29c2f5eb88980934539a8aacd76d0cf9
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-28 12:51:59 +1100

    added the fetch pet to get the details of the pets

--- Details.jsx
+++ Details.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./src/fetchPet";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
 
-export default Details
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+  const pet = results.data.pets[0];
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, ${pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+
 const localCache = {};
 
 export default function useBreedList(animal) {
